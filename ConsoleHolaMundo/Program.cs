a9fd4152352be2c2d7ee0297350e205496f632b5
ivan <p-ialvaradom@izzi.mx>
2024-01-22 11:54:06 -0600

    firts commit

---
+++ ConsoleHolaMundo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleHolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mMensaje = "Hola Mundo";
+            Console.WriteLine(mMensaje);
+            //Console.ReadKey();
+        }
+    }
+}
